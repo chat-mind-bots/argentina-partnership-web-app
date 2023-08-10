b0d364a5c793bf14438f9c0f61294466fea11cc7
Solpio <ssusolpio@gmail.com>
2023-08-11 01:08:32 +0400

    add partner

--- src/shared/components/slider/index.tsx
+++ src/shared/components/slider/index.tsx
@@ -1,13 +1,42 @@
 import React from "react";
+import { Button } from "antd";
 
 interface SliderProps {
-  children: React.ReactElement;
+  steps: string[];
 }
 
-const Slider = ({ children }: SliderProps) => {
+const Slider = ({ steps }: SliderProps) => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const handleNext = () => {
+    setActiveStep((prevState) => prevState + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevState) => prevState - 1);
+  };
   return (
     <div>
-      <div></div>
+      {steps.map((step, index) => {
+        if (activeStep === index) {
+          return step;
+        }
+      })}
+      {activeStep !== steps.length ? (
+        <div>
+          <Button
+            type={"primary"}
+            onClick={handleBack}
+            disabled={activeStep === 0}
+          >
+            Назад
+          </Button>
+          <Button type={"primary"} onClick={handleNext}>
+            {activeStep === steps.length - 1 ? "Finish" : "Вперед"}
+          </Button>
+        </div>
+      ) : (
+        <div>All steps completed - you&apos;re finished</div>
+      )}
     </div>
   );
 };
--- src/shared/partner/index.tsx
+++ src/shared/partner/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Button } from "antd";
+import Slider from "shared/components/slider";
 
 const Partner = () => {
   return (
     <div>
-      <Button type="primary">добавить бизнес</Button>
-      <Button type="primary">мои бизнесы</Button>
+      <Slider steps={["шаг 1", "шаг 2", "шыг 3"]}></Slider>
     </div>
   );
 };
