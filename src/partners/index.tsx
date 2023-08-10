961521d5d57590f961a6acbba86aa3b549bfbfab
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-10 00:37:47 -0300

    upd

--- src/partners/index.tsx
+++ src/partners/index.tsx
@@ -1,17 +1,26 @@
-import React from "react"
-import styles from "./partners.module.css"
+import React from "react";
+import styles from "./partners.module.css";
 
 // @ts-ignore
 const tg = window.Telegram.WebApp;
 
+tg.onEvent("qrTextReceived", () => {
+  tg.openLink("https://google.com");
+});
 const Partners = () => {
-    const onClose = () => {
-        tg.close()
-    }
-    const onScan = () => {
-        tg.showScanQrPopup(true)
-    }
-    return <div className={styles.body}><button onClick={onScan}>Scan</button><button onClick={onClose}>Закрыть</button></div>
-}
+  const onClose = () => {
+    tg.close();
+  };
+  const onScan = () => {
+    // tg.showScanQrPopup("menu");
+    tg.showScanQrPopup();
+  };
+  return (
+    <div className={styles.body}>
+      <button onClick={onScan}>Scan</button>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
+  );
+};
 
-export default Partners
+export default Partners;
