76829dac24f1c4a74a857f6ad0dda0afc2a698c2
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-11 12:12:01 -0300

    upd

--- src/shared/partners/index.tsx
+++ src/shared/partners/index.tsx
@@ -1,33 +1,53 @@
 import React, { useEffect } from "react";
-import styles from "shared/partners/partners.module.css";
-import { useTelegram } from "hooks/useTelegram";
+import { Button, Form, Typography } from "antd";
 import {
   WebAppProvider,
   MainButton,
   BackButton,
+  useScanQrPopup,
+  useShowPopup,
 } from "@vkruglikov/react-telegram-web-app";
 
 const Partners = () => {
-  const { tg } = useTelegram();
-  const onClose = () => {
-    tg.close();
-  };
-  const onScan = () => {
-    // tg.showScanQrPopup("menu");
-    tg.showScanQrPopup(true);
-  };
+  const [showQrPopup, closeQrPopup] = useScanQrPopup();
+  const showPopup = useShowPopup();
+  // const { tg } = useTelegram();
+  // const onClose = () => {
+  //   tg.close();
+  // };
+  // const onScan = () => {
+  //   // tg.showScanQrPopup("menu");
+  //   tg.showScanQrPopup(true);
+  // };
 
-  useEffect(() => {
-    tg.onEvent("qrTextReceived", () => {
-      tg.close();
-      tg.sendData({ data: "menu", button_text: "text" });
-    });
-  });
+  // useEffect(() => {
+  //   tg.onEvent("qrTextReceived", () => {
+  //     tg.close();
+  //     tg.sendData({ data: "menu", button_text: "text" });
+  //   });
+  // });
   return (
     <WebAppProvider>
       <MainButton />
       <BackButton />
-      <button onClick={onScan}>Scan</button>
+      <Typography.Title>Сканировать QR код</Typography.Title>
+      <button
+        onClick={() =>
+          showQrPopup(
+            {
+              text: "Наведите на QR код",
+            },
+            (text) => {
+              closeQrPopup();
+              showPopup({
+                message: text,
+              });
+            },
+          )
+        }
+      >
+        Scan
+      </button>
     </WebAppProvider>
   );
 };
