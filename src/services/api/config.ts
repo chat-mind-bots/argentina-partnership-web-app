e5334eee33bb576826fc318efd9d546fe34b5d9b
Solpio <ssusolpio@gmail.com>
2023-08-12 03:49:08 +0400

    refactor

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -1,6 +1,6 @@
 export const config = {
 	api: {
-		baseURL: process.env.BASE_URL,
+		baseURL: process.env.BACKEND_ENDPOINT,
 		// process.env.NODE_ENV === "develop" ? "http://localhost:5050/" : undefined,
 		timeout: 25000,
 	},
