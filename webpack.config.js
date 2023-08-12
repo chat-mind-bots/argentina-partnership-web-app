a423c2de208c15fe808289c2e3ee3aa1c9bb0b8f
Solpio <ssusolpio@gmail.com>
2023-08-12 04:22:10 +0400

    refactor webpack

--- webpack.config.js
+++ webpack.config.js
@@ -64,9 +64,7 @@ module.exports = {
 		new webpack.DefinePlugin({
 			"process.env.BASE_URL": JSON.stringify(process.env.REACT_BASE_URL),
 			"process.env.MODE": JSON.stringify(process.env.REACT_MODE),
-			"process.env.BACKEND_ENDPOINT": JSON.stringify(
-				process.env.REACT_BACKEND_ENDPOINT
-			),
+			"process.env.BACKEND_ENDPOINT": "http://localhost:3000",
 		}),
 		new CleanWebpackPlugin(),
 		new ForkTsCheckerWebpackPlugin(),
