a614e21ae0f191688b0b973bb202ae828440f7b2
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-12 22:42:53 -0300

    upd

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -2,7 +2,7 @@ import * as process from "process";
 
 export const config = {
 	api: {
-		baseURL: `${process.env.BASE_URL}/api/`,
+		baseURL: `https://${process.env.BASE_URL}/api/`,
 		// process.env.NODE_ENV === "develop" ? "http://localhost:5050/" : undefined,
 		timeout: 25000,
 	},
