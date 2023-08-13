788bad382c9d3edf210b614a716a29c49e62ef6d
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-13 09:27:51 -0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -5,74 +5,77 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const webpack = require("webpack");
-module.exports = {
-	mode: "development",
-	entry: "./src/index.tsx",
-	output: {
-		path: path.resolve(__dirname, "./build"),
-		filename: "[name].[hash].js",
-		publicPath: "/",
-	},
-	devServer: {
-		port: 3001,
-		static: {
-			directory: path.resolve(__dirname, "./build"),
+module.exports = (env) => {
+	console.log(env);
+	return {
+		mode: "development",
+		entry: "./src/index.tsx",
+		output: {
+			path: path.resolve(__dirname, "./build"),
+			filename: "[name].[hash].js",
+			publicPath: "/",
 		},
-		historyApiFallback: {
-			disableDotRule: true,
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(le|c)ss$/,
-				use: ["style-loader", "css-loader", "less-loader"],
+		devServer: {
+			port: 3001,
+			static: {
+				directory: path.resolve(__dirname, "./build"),
 			},
-			{
-				test: /\.(woff|woff2|otf|ttf|eot|png|jpg|jpeg|gif)$/i,
-				use: {
-					loader: "file-loader",
-					options: {
-						name: "[name].[hash].[ext]",
-						outputPath: "./assets",
+			historyApiFallback: {
+				disableDotRule: true,
+			},
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(le|c)ss$/,
+					use: ["style-loader", "css-loader", "less-loader"],
+				},
+				{
+					test: /\.(woff|woff2|otf|ttf|eot|png|jpg|jpeg|gif)$/i,
+					use: {
+						loader: "file-loader",
+						options: {
+							name: "[name].[hash].[ext]",
+							outputPath: "./assets",
+						},
 					},
 				},
-			},
-			{
-				test: /\.svg$/,
-				use: ["@svgr/webpack"],
-			},
-			{
-				test: /\.(js|ts)x?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
+				{
+					test: /\.svg$/,
+					use: ["@svgr/webpack"],
 				},
-			},
-		],
-	},
-	resolve: {
-		extensions: [".tsx", ".ts", ".js", ".jsx", "less", "css", "scss"],
-		modules: ["node_modules", "src"],
-		alias: {
-			process: "process/browser",
+				{
+					test: /\.(js|ts)x?$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+					},
+				},
+			],
 		},
-	},
-	plugins: [
-		new Dotenv(),
-		new HtmlWebpackPlugin({
-			title: "Finance App",
-			template: "./public/index.html",
-		}),
-		new webpack.DefinePlugin({
-			"process.env.BASE_URL": JSON.stringify(process.env.BASE_URL),
-			"process.env.MODE": JSON.stringify(process.env.MODE),
-			"process.env.BACKEND_PORT": JSON.stringify(process.env.BACKEND_PORT),
-			"process.env.BACKEND_ENDPOINT": JSON.stringify(
-				process.env.BACKEND_ENDPOINT
-			),
-		}),
-		new CleanWebpackPlugin(),
-		new ForkTsCheckerWebpackPlugin(),
-	],
+		resolve: {
+			extensions: [".tsx", ".ts", ".js", ".jsx", "less", "css", "scss"],
+			modules: ["node_modules", "src"],
+			alias: {
+				process: "process/browser",
+			},
+		},
+		plugins: [
+			// new Dotenv(),
+			new HtmlWebpackPlugin({
+				title: "Finance App",
+				template: "./public/index.html",
+			}),
+			new webpack.DefinePlugin({
+				"process.env.BASE_URL": JSON.stringify(process.env.BASE_URL),
+				"process.env.MODE": JSON.stringify(process.env.MODE),
+				"process.env.BACKEND_PORT": JSON.stringify(process.env.BACKEND_PORT),
+				"process.env.BACKEND_ENDPOINT": JSON.stringify(
+					process.env.BACKEND_ENDPOINT
+				),
+			}),
+			new CleanWebpackPlugin(),
+			new ForkTsCheckerWebpackPlugin(),
+		],
+	};
 };
