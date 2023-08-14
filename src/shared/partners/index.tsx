4cbe1f1123a183657d50413ff9775666156674c9
Petr Molchanov <p.molchanov@profsoft.pro>
2023-08-13 21:45:38 -0300

    page loader

--- src/shared/partners/index.tsx
+++ src/shared/partners/index.tsx
@@ -11,15 +11,14 @@ import { useTelegram } from "hooks/useTelegram";
 import { get } from "services/api";
 import { JsonplaceholderResp } from "shared/business/data";
 import { useLoaderData } from "react-router-dom";
-import PageLoader from "shared/components/page-loader";
 
-export async function loader(): Promise<JsonplaceholderResp> {
-	const data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
-		(response) => response.json()
-	);
-	console.log("Finish");
-	return data;
-}
+// export async function loader(): Promise<JsonplaceholderResp> {
+// 	const data = await fetch("https://jsonplaceholder.typicode.com/todos/1").then(
+// 		(response) => response.json()
+// 	);
+// 	console.log("Finish");
+// 	return data;
+// }
 
 export function Component() {
 	const { user } = useTelegram();
@@ -29,52 +28,48 @@ export function Component() {
 	let data = useLoaderData() as string;
 	return (
 		<WebAppProvider>
-			<PageLoader />
-			{/*<MainButton />*/}
-			{/*<BackButton />*/}
-			{/*<Typography.Title>Сканировать QR код</Typography.Title>*/}
-			{/*<button*/}
-			{/*	onClick={() =>*/}
-			{/*		showQrPopup(*/}
-			{/*			{*/}
-			{/*				text: "Наведите на QR код",*/}
-			{/*			},*/}
-			{/*			(text) => {*/}
-			{/*				closeQrPopup();*/}
-			{/*				if (text.includes("user-code-")) {*/}
-			{/*					const code = text.split("user-code-")[1];*/}
-			{/*					const url = `user-codes/${code}`;*/}
-			{/*					get<{*/}
-			{/*						status: "authorized" | "reject";*/}
-			{/*					}>(url, { query: { id: user.id } })*/}
-			{/*						.then(async (data) => {*/}
-			{/*							if (data.status === "authorized") {*/}
-			{/*								await showPopup({*/}
-			{/*									message: "Код успешно активирован",*/}
-			{/*								});*/}
-			{/*							} else {*/}
-			{/*								await showPopup({*/}
-			{/*									message:*/}
-			{/*										"Код был актвирован ранее, или его срок действия истек. Просканируйте новый код",*/}
-			{/*								});*/}
-			{/*							}*/}
-			{/*						})*/}
-			{/*						.catch(async () => {*/}
-			{/*							await showPopup({*/}
-			{/*								message:*/}
-			{/*									"Произошла ошибка, попробуйте позже, или поробуйте просканировать новый код",*/}
-			{/*							});*/}
-			{/*						});*/}
-			{/*				}*/}
-			{/*			}*/}
-			{/*		)*/}
-			{/*	}*/}
-			{/*>*/}
-			{/*	Scan 2.0*/}
-			{/*</button>*/}
+			<Typography.Title>Сканировать QR код</Typography.Title>
+			<button
+				onClick={() =>
+					showQrPopup(
+						{
+							text: "Наведите на QR код",
+						},
+						(text) => {
+							closeQrPopup();
+							if (text.includes("user-code-")) {
+								const code = text.split("user-code-")[1];
+								const url = `user-codes/${code}`;
+								get<{
+									status: "authorized" | "reject";
+								}>(url, { query: { id: user.id } })
+									.then(async (data) => {
+										if (data.status === "authorized") {
+											await showPopup({
+												message: "Код успешно активирован",
+											});
+										} else {
+											await showPopup({
+												message:
+													"Код был актвирован ранее, или его срок действия истек. Просканируйте новый код",
+											});
+										}
+									})
+									.catch(async () => {
+										await showPopup({
+											message:
+												"Произошла ошибка, попробуйте позже, или поробуйте просканировать новый код",
+										});
+									});
+							}
+						}
+					)
+				}
+			>
+				Scan 2.0
+			</button>
 		</WebAppProvider>
 	);
 }
 
 Component.displayName = "Partners Page";
-// export default Partners;
