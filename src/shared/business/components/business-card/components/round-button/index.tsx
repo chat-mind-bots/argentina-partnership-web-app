d4749271e052b82ea507c25979f45dbe3985bcc0
Solpio <ssusolpio@gmail.com>
2023-08-30 15:25:02 +0400

    add colors

--- src/shared/business/components/business-card/components/round-button/index.tsx
+++ src/shared/business/components/business-card/components/round-button/index.tsx
@@ -3,13 +3,18 @@ import styles from "./round-button.module.css";
 interface RoundButtonProps {
 	title: string;
 	callBack: () => void;
+	isActive?: boolean;
 	logo?: React.ReactNode;
 }
 
-function RoundButton({ logo, callBack, title }: RoundButtonProps) {
+function RoundButton({ logo, callBack, title, isActive }: RoundButtonProps) {
 	return (
 		<div onClick={callBack} className={styles.button}>
-			{logo && <div className={styles.logo}>{logo}</div>}
+			{logo && (
+				<div className={`${styles.logo} ${isActive && styles.active}`}>
+					{logo}
+				</div>
+			)}
 			<div className={styles.buttonTitle}>{title}</div>
 		</div>
 	);
