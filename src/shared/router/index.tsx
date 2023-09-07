c3f53d5461d755c9f3eb64c3ce198b196769fbb2
Petr Molchanov <p.molchanov@profsoft.pro>
2023-09-07 08:46:31 -0300

    suspense

--- src/shared/router/index.tsx
+++ src/shared/router/index.tsx
@@ -1,52 +1,57 @@
-import { createBrowserRouter } from "react-router-dom";
-import React from "react";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import React, { Suspense } from "react";
+import PageLoader from "../components/page-loader";
 
 export const router = createBrowserRouter([
 	{
 		path: "",
-		Component() {
-			return <div>Home page</div>;
-		},
-	},
-	{
-		path: "qr-check",
-		lazy: () => import("shared/qr-code/qr-check"),
-	},
-	{
-		path: "qr-generate",
-		lazy: () => import("shared/qr-code/qr-generate"),
-	},
-	{
-		path: "home",
-		lazy: () => import("shared/home/components"),
-	},
-	{
-		path: "my-payments",
-		lazy: () => import("shared/payment/components/history"),
-	},
-	{
-		path: "payment/:paymentId",
-		lazy: () => import("shared/payment/components/payment-info"),
-	},
-	{
-		path: "top-up",
-		lazy: () => import("shared/payment/components/top-up"),
-	},
-	{
-		path: "/create/business",
-		lazy: () => import("shared/business/components/create-business-form"),
-	},
-	{
-		path: "/partner/:userId/business/:businessId/",
-		lazy: () => import("shared/business/components/business-card"),
-	},
-	{
-		path: "/partner/:userId/business/:businessId/update",
-		lazy: () => import("shared/business/components/create-business-form"),
-	},
-	{
-		path: "/partners",
-		lazy: () => import("shared/business/components/list"),
+		element: (
+			<Suspense fallback={<PageLoader />}>
+				<Outlet />
+			</Suspense>
+		),
+		children: [
+			{
+				path: "qr-check",
+				lazy: () => import("shared/qr-code/qr-check"),
+			},
+			{
+				path: "qr-generate",
+				lazy: () => import("shared/qr-code/qr-generate"),
+			},
+			{
+				path: "home",
+				lazy: () => import("shared/home/components"),
+			},
+			{
+				path: "my-payments",
+				lazy: () => import("shared/payment/components/history"),
+			},
+			{
+				path: "payment/:paymentId",
+				lazy: () => import("shared/payment/components/payment-info"),
+			},
+			{
+				path: "top-up",
+				lazy: () => import("shared/payment/components/top-up"),
+			},
+			{
+				path: "/create/business",
+				lazy: () => import("shared/business/components/create-business-form"),
+			},
+			{
+				path: "/partner/:userId/business/:businessId/",
+				lazy: () => import("shared/business/components/business-card"),
+			},
+			{
+				path: "/partner/:userId/business/:businessId/update",
+				lazy: () => import("shared/business/components/create-business-form"),
+			},
+			{
+				path: "/partners",
+				lazy: () => import("shared/business/components/list"),
+			},
+		],
 	},
 	{
 		path: "*",
