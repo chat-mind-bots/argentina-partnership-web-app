ae033ae04a100e1597c968e45ba01b8027f62cd8
Solpio <ssusolpio@gmail.com>
2023-09-08 23:09:18 +0400

    add icon

--- src/shared/home/components/navigation/index.tsx
+++ src/shared/home/components/navigation/index.tsx
@@ -3,6 +3,7 @@ import styles from "shared/home/components/navigation/navigation.module.less";
 import { ReactComponent as Business } from "public/assets/icons/business.svg";
 import { ReactComponent as AngleRight } from "public/assets/icons/angle-right.svg";
 import { ReactComponent as Qr } from "public/assets/icons/qr.svg";
+import { ReactComponent as Partnership } from "public/assets/icons/partnership.svg";
 import { ReactComponent as TariffPlans } from "public/assets/icons/tariff.svg";
 import NavigationButton from "shared/home/components/navigation/navigation-button";
 import Card from "shared/components/card";
@@ -54,7 +55,7 @@ const Navigation = () => {
 				onClick={() => navigate("/partnership")}
 				logo={
 					<div className={styles.logo}>
-						<Qr />
+						<Partnership />
 					</div>
 				}
 			>
