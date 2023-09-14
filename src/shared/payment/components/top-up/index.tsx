43494fcbe56c5ae5b1baeaef970d504d6f6ef88f
Solpio <ssusolpio@gmail.com>
2023-09-14 04:03:29 +0400

    refactor top-up

--- src/shared/payment/components/top-up/index.tsx
+++ src/shared/payment/components/top-up/index.tsx
@@ -5,19 +5,11 @@ import {
 	useShowPopup,
 	WebAppProvider,
 } from "@vkruglikov/react-telegram-web-app";
-import {
-	Await,
-	defer,
-	useAsyncValue,
-	useLoaderData,
-	useNavigate,
-} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ReactComponent as History } from "public/assets/icons/history.svg";
 import ContentLayout from "shared/components/content-layout";
 import InputText from "shared/components/input/input-text";
 import Description from "shared/components/description";
-import { get } from "services/api";
-import { User } from "shared/home/interfaces/user.interface";
 import { createPayment } from "shared/payment/services/data";
 import { CurrenciesEnum } from "shared/payment/interfaces/currencies.enum";
 import Select from "shared/components/select";
@@ -26,13 +18,6 @@ import { NetworksEnum } from "shared/payment/interfaces/networks.enum";
 import PageLoader from "shared/components/page-loader";
 import styles from "shared/payment/components/top-up/top-up.module.less";
 import { PaymentContext } from "shared/context/payment/payment.context";
-
-export async function loader() {
-	// @ts-ignore
-	const user = window.Telegram.WebApp.initDataUnsafe?.user;
-	const userDataPromise = get<User>(`user/${user.id}`, {});
-	return defer({ userDataPromise });
-}
 
 function TopUp() {
 	const [value, setValue] = useState<{
@@ -45,32 +30,28 @@ function TopUp() {
 	const [progress, setProgress] = useState(false);
 
 	const showPopup = useShowPopup();
-	const data = useAsyncValue() as User;
 
-	const sendPayment = useCallback(
-		(amount: number, network: NetworksEnum) => {
-			createPayment(data._id, {
-				currency: CurrenciesEnum.USDT,
-				amount,
-				method: network,
-			})
-				.then(async () => {
-					await showPopup({
-						message: "Запрос на пополнение успешно создан создан",
-					});
-					updatePayments && updatePayments();
-					setProgress(false);
-				})
-				.catch(async () => {
-					await showPopup({
-						message: "Во время создания запроса на пополнение произошла ошибка",
-					});
-					updatePayments && updatePayments();
-					setProgress(false);
+	const sendPayment = useCallback((amount: number, network: NetworksEnum) => {
+		createPayment({
+			currency: CurrenciesEnum.USDT,
+			amount,
+			method: network,
+		})
+			.then(async () => {
+				await showPopup({
+					message: "Запрос на пополнение успешно создан создан",
 				});
-		},
-		[data]
-	);
+				updatePayments && updatePayments();
+				setProgress(false);
+			})
+			.catch(async () => {
+				await showPopup({
+					message: "Во время создания запроса на пополнение произошла ошибка",
+				});
+				updatePayments && updatePayments();
+				setProgress(false);
+			});
+	}, []);
 	const navigation = useNavigate();
 
 	const toHome = useCallback(() => {
@@ -144,12 +125,9 @@ function TopUp() {
 }
 
 export function Component() {
-	const data = useLoaderData() as { userDataPromise: User };
 	return (
 		<Suspense fallback={<PageLoader />}>
-			<Await resolve={data.userDataPromise}>
-				<TopUp />
-			</Await>
+			<TopUp />
 		</Suspense>
 	);
 }
--- src/shared/payment/services/data/index.ts
+++ src/shared/payment/services/data/index.ts
@@ -6,8 +6,8 @@ import {
 } from "shared/payment/interfaces/payment.interface";
 import { GetPaymentDto } from "shared/payment/interfaces/get-payment.dto";
 
-export const createPayment = (userId: string, body: CreatePaymentInterface) =>
-	post<PaymentInterface>("payment", { body, query: { userId } });
+export const createPayment = (body: CreatePaymentInterface) =>
+	post<PaymentInterface>("payment", { body });
 
 export const getMyPayments = (query: GetPaymentDto) =>
 	get<PaymentInterfaceDto>("payment", { query: { ...query } });
