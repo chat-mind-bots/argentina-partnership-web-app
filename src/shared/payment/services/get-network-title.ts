b399a128fd3f0af887d82b80fe514e06401a5d8a
Solpio <ssusolpio@gmail.com>
2023-09-26 01:57:37 +0400

    remove solana and eth

--- src/shared/payment/services/get-network-title.ts
+++ src/shared/payment/services/get-network-title.ts
@@ -4,10 +4,10 @@ export const getNetworkTitle = (network: NetworksEnum) => {
 	switch (network) {
 		case NetworksEnum.BEP20:
 			return "BNB Smart Chain (BEP20)";
-		case NetworksEnum.ERC20:
-			return "Ethereum (ERC20)";
-		case NetworksEnum.SOL:
-			return "Solana";
+		// case NetworksEnum.ERC20:
+		// 	return "Ethereum (ERC20)";
+		// case NetworksEnum.SOL:
+		// 	return "Solana";
 		case NetworksEnum.TRC20:
 			return "Tron (TRC20)";
 	}
